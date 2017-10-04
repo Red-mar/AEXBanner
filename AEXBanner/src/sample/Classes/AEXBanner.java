c50062fce750395e037b144236d0b8f4de96fe18
Riet,Redmar R.F. van de <r.vanderiet@student.fontys.nl>
2017-10-04 13:05:41 +0200

    class implementation

--- AEXBanner/src/sample/Classes/AEXBanner.java
+++ AEXBanner/src/sample/Classes/AEXBanner.java
@@ -1,6 +1,8 @@
 package sample.Classes;
 
 public class AEXBanner {
+    BannerController bannerController;
+
     public void setKoersen(String koersen){
 
     }
--- AEXBanner/src/sample/Classes/BannerController.java
+++ AEXBanner/src/sample/Classes/BannerController.java
@@ -1,7 +1,11 @@
 package sample.Classes;
 
 public class BannerController {
+    AEXBanner aexBanner;
+    IEffectenbeurs effectenbeurs;
+
     public BannerController(AEXBanner banner) {
+        this.aexBanner = banner;
     }
 
     public void stop(){
